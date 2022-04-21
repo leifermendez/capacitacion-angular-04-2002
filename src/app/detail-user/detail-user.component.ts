bdfabecf99bf66b2f4b3f86b884e1f756c5fb64e
Leifer Mendez <leifer33@gmail.com>
2022-04-21 22:59:50 +0200

    pipe. filter

--- src/app/detail-user/detail-user.component.ts
+++ src/app/detail-user/detail-user.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-detail-user',
   templateUrl: './detail-user.component.html',
-  styleUrls: ['./detail-user.component.css']
+  styleUrls: ['./detail-user.component.css'],
 })
 export class DetailUserComponent implements OnInit {
+  userForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    completed: new FormControl(true),
+    id: new FormControl(1),
+    userId: new FormControl(1),
+  });
+  @Input() listaUsuarios:[number, string] = [1, 'leifer']
+  constructor(private userService: UserService) {}
 
-  userActualEnDetail = this.sharedService.userSelected;
+  ngOnInit(): void {}
 
-  constructor(private sharedService:SharedService) { }
-
-  ngOnInit(): void {
+  addUser():void{
+    const userNew = this.userForm.value;
+    this.userService.create(userNew)
   }
-
 }
